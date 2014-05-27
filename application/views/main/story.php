7a2232f9d0d89921856bfce119517a61865451e7
Neil Martin <n@clayza.com>
2014-05-27 13:38:41 -0400

    added story copy, fixed link styling

--- application/views/main/story.php
+++ application/views/main/story.php
@@ -44,28 +44,33 @@
 		
 	</div>
 	<div class="medium-8 columns">
-		<p class="p2">The momentum behind wearable technology is immense and it is growing fast. In the past two weeks alone we have seen Google release Android Wear, an extension of the Android operating system for smartwatches; Samsung launch its Tizen SDK; Intel complete their purchase of Basis Science; and Facebook acquire VR headset Oculus for $2 billion dollars.
-		</p>
+		<p class="p2">In life we are nothing without our identity. We need ID to drive, to buy food and to put a roof over our heads. We are at the mercy of passwords, PINs and policy numbers to function and survive. This is especially the case in a digital age where computers and smartphones require a series of characters and numbers to determine who you are to grant you access. But the thing about passwords is that they are easy to forget, a pain to enter and are extremely vulnerable to security threats. In a perfect world, our connected machines would be able to discern who we are and grant us access to our email, our house and even our bank accounts simply because we are alive and within proximity. This is the dream that Toronto-based Bionym wants to make a reality with its wearable technology, the Nymi.</p>
 
-		<p class="p2">Here at home, Canada is proving to be a force to be reckoned with in wearable tech. Toronto and the surrounding areas are an epicenter for wearables especially if we look at the number of startups creating game changing devices that will leave a lasting impression on how we live our everyday lives</p>
+		<p class="p2">The Nymi is a wristband that uses your unique cardiac rhythm or your heartbeat as an identifier. Once activated, which is done through a long tap on the Nymi to close the circuit, the device connects to other authorized devices and applications using Bluetooth.</p> 
 
-		<p class="p2">In the past three months, you as a community have shown a tremendous amount of curiosity, enthusiasm and dedication to this space. You have proven that Wearable Wednesday Toronto is more than just a one-day a month occurrence. It is a movement!</p>
+		 <p class="p2">As security is paramount when it comes to digital identity, Bionym has created the Nymi with a three-factor security system. In order to authenticate, users must be wearing their Nymi, your unique heartbeat along with an Authorized Authentication Device like your smartphone.</p> 
 
-		<p class="p2">We are a Movement.</p>
+		<p class="p2">But the Nymi goes far beyond a better password. Its motion sensing technology using an accelerometer empowers the user to use simple gestures to perform task-specific commands. These become important when you want to use the Nymi to access a specific device in an home that has a growing number of smart things.</p> 
 
-		<p class="p2">We are Wearables!</p>
+		<p class="p2">The potential of Nymi is vast but the strength of the device will reside in the applications that use it. And BIonym has been busy working on the necessary partnerships and growing the required developer ecosystem to create a world that is Nymi-friendly.</p>
 
-		<p class="p2">To reflect this I am excited to announce that Wearable Wednesday Toronto has rebranded to We Are Wearables.</p>
+		<p class="p2">Back in February of this year, Binonym announced that the Nymi will have its own secure BItcoin wallet as one of its killer apps. The Bitcoin wallet will let you store your private key necessary to send and withdraw funds from your account on your Nymi. This adds a level of security both through the use of the heartbeat authentication and the fact that your private key won’t be stored on your computers hard drive where it may be more easy to steal.</p>
 
-		<p class="p2">Under this new brand we will be able to widen our scope to allow us to even better meet our mission to collectively foster the necessary ecosystem around this new tech to move this industry forward and make it a success</p>
+		<p class="p2">Bionym also announced its partnership with password manager, Passwordbox, which will quickly get users access to a wide variety of online portals and accounts just by wearing the Nymi.</p> 
 
-		<p class="p2">We will continue to have our regularly monthly WWTO events, which now stands for We Are Wearables Toronto. There is no change to this meetup outside of the name. We are still dedicated to bringing together experts and startups in the wearable space for some fantastic discussions and demos.</p>
+		<p class="p2">We sat down with the team from Bionym in a panel-style interview at our April Toronto event just after Bionym took the stage to give the Toronto audience a world exclusive demo of the Nymi in action. The team talked candidly about some of the challenges in manufacturing a wearable as well as why the Nymi is a wearable device not a solution integrated into other devices like smartphones.</p>
 
-		<p class="p2">But you can expect to see other events pop-up in this city and possibly others. We are also taking WWTO on the road to bring workshops, lunch-and-learns and presentations to companies, brands, and agencies who want to have this discussion with their teams or clients.</p>
+		<p class="p2">“The thing that is really key to what we are doing is this concept of persistent identity and if we put this in a smartphone or even smart cards or things that you handle it becomes transactional when that thing only knows its you are holding it and when you let go it no longer knows that it is you,” explained CEO and Co-founder of Bionym Dr. Karl Martin.  “We found that there is alot of value in something that you put on and authenticates you and it persists for the rest of the day”.</p>
 
-		<p class="p2">And we will be launching our official website in the next couple of weeks which will acts a hub for the community and an online resource for wearable tech.</p>
+		<p class="p2">Of course wearing a device that can identify you and your behavior is quite lucrative to marketers and brands who use this data to drive their business. We also talked to the team about the privacy concerns users may have about their data while wearing the Nymi and how Bionym is handling this.</p> 
 
-		<p class="p2">Thank you for making this happen. We Are Wearables, and I am excited to see where we all take this.
+		<p class="p2">“There is a major paradigm that we are changing,” said Martin. “All the ways of tracking now are adversarial with things like cookies. We designed the Nymi with the concept of Privacy by Design which is designing the product with user-based privacy principles from the ground up. Everything is opt-in. Without the opt-in process no one knows who you are”.</p>
+
+		<p class="p2">Bionym President Andrew D’Souza sees users giving up their data if they see value. “We have the view that data is going to be a form of currency,” explained D’Souza. “We are going to see a shift where the power goes back to the consumer. It will be up to the service provider to create enough value for people to share that data.  I am ok with giving away information if I can speed through the airport or get a really great personalized experience when I go into a retailer or restaurant”.</p>
+
+		<p class="p2">As wearable devices like the Nymi become more widely adopted it will be interesting to see what value service providers offer to these customers to convince them of sharing their data. But one this is clear, this process will happen more seamless and less intrusive than the current loyalty card. Bionym’s marketing video promises a world where we walk up to cars, tills and hotel room doors and the transaction occurs with little to no effort and definitely without the use of PINs and passwords.</p>   
+
+		<p class="p2">Bionym is currently in <a href="http://www.getnymi.com/">pre-orders</a> for the Nymi which is selling for $79 with a shipping date of mid-2014.
 		</p>
 	</div>
 	
